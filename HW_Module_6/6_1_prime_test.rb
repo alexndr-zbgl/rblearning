d61b4322e3340004fc893313a87661adf3149e1f
Alex Zbgl <sasha.zbgl@gmail.com>
2020-07-08 22:03:08 +0300

    Module 6 ready

---
+++ HW_Module_6/6_1_prime_test.rb
@@ -0,0 +1,42 @@
+﻿# Програма для перевірки, чи дане число є простим.
+# використовується решето Ерастрофена з оптимізацією
+# Оптимізація полягає в початку висіювання наступного
+# не викресленого числа з його квадрату.
+
+# Ви маєте зреалізувати логіку для формування масиву-шерета,
+# його просіювання та виведення інформації, чи вказане число є простим.
+
+class Calculator
+  attr_accessor :sieve
+
+  
+  def setup_prime n
+    @sieve = (0..n).to_a
+    @sieve[0] = nil
+    @sieve[1] = nil
+  end
+
+ 
+  def sieve_prime_upto n
+    @sieve.each do |p|
+    next unless p
+
+    
+    break if p*p > n.to_i
+
+    (p*p).step(n.to_i ,p) { |j| @sieve[j] = nil }
+  end
+
+    
+    @sieve.compact
+  end
+
+  
+  def sieve_prime? n
+    setup_prime n
+    sieve_prime_upto n
+    sieve_prime_upto(n).include?(n)
+  end
+end
+input = gets.chomp.to_i
+puts Calculator.new.sieve_prime?(input)
